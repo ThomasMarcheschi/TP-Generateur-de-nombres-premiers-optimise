8262f3bc805052df3d1bace6039a1b329f3ae2e7
Jovené Kévin <kevin@MacBook-Pro-de-Jovene.local>
2025-11-14 14:08:28 +0100

    :sparkles: Config des fichiers :sparkles:

---
+++ app-prime/src/hooks/usePrimeAlea.jsx
@@ -0,0 +1,7 @@
+import { numberSchema } from "../schemas/numberSchema";
+
+export async function fetchNumberAlea() {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  const raw = { number: Math.floor(Math.random() * 1000) };
+  return numberSchema.parse(raw);
+}

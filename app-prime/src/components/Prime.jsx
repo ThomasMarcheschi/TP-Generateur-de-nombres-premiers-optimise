68a9ecd2de4135168e5de92d6b3de3d8758fab92
MarcheschiThomas <marcheschi.thomas@gmail.com>
2025-11-14 11:26:38 +0100

    Debut tp

---
+++ app-prime/src/components/Prime.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Prime() {
+  return <div>Prime</div>;
+}
+
+export default Prime;
---
+++ app-prime/src/pages/PrimePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PrimePage() {
+  return <div>PrimePage</div>;
+}
+
+export default PrimePage;
